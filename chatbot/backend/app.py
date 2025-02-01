a7aa6915034d54cf5920095e20db5287b1d6a258
Adriano Arias <adrianoarias703@gmail.com>
2025-02-01 16:16:16 -0500

    setup

--- chatbot/backend/app.py
+++ chatbot/backend/app.py
@@ -1,9 +1,37 @@
 # backend/app.py
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import os
+
 
 app = Flask(__name__)
 CORS(app)
+
+
+############Database############
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, "users.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.email
+
+with app.app_context():
+    db.create_all()
+
+
+
+################################
 
 @app.route('/')
 def home():
