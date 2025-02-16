336750f627fcf8e538ab37c34952c17d4d5a21aa
Matis Luzi <matisluzi@gmail.com>
2025-02-16 16:28:18 -0500

    add output.css to gitignore

--- chatbot/frontend/src/pages/AppContent.jsx
+++ chatbot/frontend/src/pages/AppContent.jsx
@@ -3,7 +3,6 @@ import "../css/output.css";
 
 import { ArrowCircleUp } from "@phosphor-icons/react";
 // component imports
-import ChatsSideMenu from "../components/ChatsSideMenu";
 import Message from "../components/Message";
 import Suggestion from "../components/Suggestion";
 
@@ -78,7 +77,7 @@ function AppContent() {
 
   return (
     <div className="max-w-3xl mx-auto p-4 pt-0 flex flex-col justify-center h-full text-base">
-        {/* <ChatsSideMenu chats={[{ name: "Chat 1" }, { name: "Chat 2" }]} /> */}
+      {/* <ChatsSideMenu chats={[{ name: "Chat 1" }, { name: "Chat 2" }]} /> */}
 
       {!isConnected && (
         <div className="error-banner">
@@ -87,9 +86,7 @@ function AppContent() {
       )}
 
       {/* Header */}
-      <h1 className="text-3xl font-bold m-2">
-        What would you like to know?
-      </h1>
+      <h1 className="text-3xl font-bold m-2">What would you like to know?</h1>
 
       {/* Suggestions (if no messages) */}
       {messages.length === 0 ? (
