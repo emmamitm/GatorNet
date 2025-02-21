b4f3270cb3652a8f8b950bb01a927aa3114899fc
Matis Luzi <matisluzi@gmail.com>
2025-02-21 06:49:32 -0500

    user icon on top bar logs user out when clicked

--- chatbot/frontend/src/components/TopBar.jsx
+++ chatbot/frontend/src/components/TopBar.jsx
@@ -1,19 +1,22 @@
 import { UserCircle } from "@phosphor-icons/react";
+import { useAuth } from "../auth/AuthContext";
 
 export function TopBar({ children }) {
+    const { logout } = useAuth();
+
     return (
         <div className="flex flex-col w-screen min-h-screen">
             <div className="bg-blue-600 w-full flex justify-between items-center h-14">
                 <h1 className="text-4xl font-bold text-white pl-4 p-2">
                     GatorNet
                 </h1>
-                <div className="pr-4">
+                <button className="pr-4" onClick={logout}>
                     <UserCircle
                         size={40}
                         weight="fill"
                         className="fill-orange-400"
                     />
-                </div>
+                </button>
             </div>
             <div className="bg-orange-400 h-2"></div>
             <div className="flex-1 flex ">{children}</div>
